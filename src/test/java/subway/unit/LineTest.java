e33a1cadefb61b34b604b9d251403ba9778983d7
o3ozzvb <ou1112@naver.com>
2024-02-04 21:52:31 +0900

    fix : 테스트 수정 (distance update)

--- src/test/java/subway/unit/LineTest.java
+++ src/test/java/subway/unit/LineTest.java
@@ -8,13 +8,13 @@ import subway.domain.entity.Line;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class LineTest {
     @Test
     void addSection() {
         // line 인스턴스를 만들고
         // addSection 을 호출했을 때
-
         Line line = new Line("2호선", "green");
 
         Station upStation = new Station("강남역");
@@ -23,17 +23,20 @@ class LineTest {
 
         int distance = 10;
 
-        line.addSection(upStation, downStation, distance);
-        line.addSection(downStation, newStation, distance);
+        line.addSection(new Section(line, upStation, downStation, distance));
+        line.addSection(new Section(line, downStation, newStation, distance));
 
-        assertThat(line.getSections()).hasSize(2);
+        assertAll(
+                () -> assertThat(line.getSections()).hasSize(2),
+                () -> assertThat(line.getDistance()).isEqualTo(20)
+        );
+
     }
 
     @Test
     void deleteSection() {
         // line 인스턴스를 만들고
         // deleteSection 을 호출했을 때
-
         Line line = new Line("2호선", "green");
 
         Station upStation = new Station("강남역");
@@ -41,13 +44,17 @@ class LineTest {
         Station newStation = new Station("역삼역");
 
         int distance = 10;
-
-        line.addSection(upStation, downStation, distance);
-        line.addSection(downStation, newStation, distance);
+        line.addSection(new Section(line, upStation, downStation, distance));
+        line.addSection(new Section(line, downStation, newStation, distance));
 
         List<Section> sections = line.getSections();
         Section section = sections.get(sections.size() - 1);
         line.deleteSection(section);
-        assertThat(line.getSections()).hasSize(1);
+
+
+        assertAll(
+                () -> assertThat(line.getSections()).hasSize(1),
+                () -> assertThat(line.getDistance()).isEqualTo(10)
+        );
     }
 }
